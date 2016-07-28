7e7e9d6eebe26d25dfd08b3df1bb8962620b5beb
Markus Graube <markus.graube@tu-dresden.de>
2016-07-28 22:55:43 +0200

    export in different files and using argparse

--- client.py
+++ client.py
@@ -2,46 +2,23 @@ import sys
 
 from opcua import Client
 from xml.dom import minidom
+import argparse
 import datetime
 
-nodes = []
-types = {}
-
-def iterater_over_child_nodes(parent):
+def iterater_over_child_nodes(parent, namespace, nodes, visited):
     for child in parent.get_children(refs=31):
-        if child not in nodes:
-            nodes.append(child)
+        if child not in visited:
+            visited.append(child)
             ns = child.nodeid.NamespaceIndex
-            try:
-                types[ns]['classes'][str(child.get_node_class())] += 1
-            except KeyError:
-                types[ns]['classes'][str(child.get_node_class())] =1
-            iterater_over_child_nodes(child)
+            if ns in namespace:
+                nodes[ns].append(child)
+            iterater_over_child_nodes(child, namespace, nodes, visited)
 
-if __name__ == "__main__":
 
-    client_url = "opc.tcp://localhost:16664"
-    client = Client(client_url)
-    
-    namespace = 2
-
-    try:
-        client.connect()
-    except Exception:
-        print("No connection established. Exiting ...")
-        sys.exit()
-
-    print("Client connected to %s" % client_url)
-
-    for ns in client.get_namespace_array():
-        types[client.get_namespace_index(ns)] = {'url': ns, 'classes': {}}
-    root = client.get_root_node()
-
-    iterater_over_child_nodes(root)
-
+def create_node_xml(nodes, namespace, namespace_array, server_url, outputFile):
     doc = minidom.Document()
-    
-    doc.appendChild(doc.createComment("Exported node xml from %s - ns: %d - %s" % (client_url, namespace, datetime.datetime.now())))
+
+    doc.appendChild(doc.createComment("Exported node xml from %s - ns: %d - %s" % (server_url, namespace, datetime.datetime.now())))
     xml_uanodeset = doc.createElement("UANodeSet")
     doc.appendChild(xml_uanodeset)
 
@@ -49,14 +26,13 @@ if __name__ == "__main__":
     xml_uanodeset.setAttributeNS("xsd", "xmlns:xsd", "http://www.w3.org/2001/XMLSchema")
     xml_uanodeset.setAttributeNS("", "xmlns", "http://opcfoundation.org/UA/2011/03/UANodeSet.xsd")
     xml_uanodeset.setAttribute("LastModified", str(datetime.datetime.now()))
-    
-    
-    xml_namespace_uris = xml_uanodeset.appendChild(doc.createElement("NamespaceUris"))
-    for ns in client.get_namespace_array():
-        xml_namespace_uris.appendChild(doc.createElement("Uri")).appendChild(doc.createTextNode(ns))
-        
 
-    for node in nodes:
+
+    xml_namespace_uris = xml_uanodeset.appendChild(doc.createElement("NamespaceUris"))
+    for ns in namespace_array:
+        xml_namespace_uris.appendChild(doc.createElement("Uri")).appendChild(doc.createTextNode(ns))
+
+    for node in nodes[namespace]:
         xml_node = xml_uanodeset.appendChild(doc.createElement("UA" + node.get_node_class()._name_))
         xml_node.setAttribute("NodeId", node.nodeid.to_string())
         xml_node.setAttribute("BrowseName", node.get_browse_name().to_string())
@@ -95,16 +71,69 @@ if __name__ == "__main__":
         except:
             pass
 
+    f = open(outputFile, 'w')
+    doc.writexml(f, indent='\t', newl='\r\n',)
+    f.close()
 
-    print(doc.toprettyxml())
 
-    for (index, ns) in types.items():
-        count = 0
-        print("Namespace %d (%s)" % (index, ns['url']))
-        for (c,val) in ns['classes'].items():
-            count += val
-            print("\t%s:\t%d" % (c,val))
-        print("\tTOTAL in namespace: %d" % count)
-    print("TOTAL in server: %d" % len(nodes))
+def export_node_xml(server_url="opc.tcp://localhost:16664", namespace=None):
+    client = Client(server_url)
+    try:
+        client.connect()
+    except Exception:
+        print("No connection established. Exiting ...")
+        sys.exit()
 
+    print("Client connected to %s" % server_url)
+
+
+
+    if not namespace:
+        namespace = []
+        for ns in client.get_namespace_array():
+            namespace.append(client.get_namespace_index(ns))
+
+
+    root = client.get_root_node()
+
+    nodes = {}
+    visited = []
+    for ns in namespace:
+        nodes[ns] = []
+
+    namespace_array = client.get_namespace_array()
+    iterater_over_child_nodes(root, namespace, nodes, visited)
+
+
+    for ns in namespace:
+        create_node_xml(nodes, ns, namespace_array, server_url, "ns%d.xml" % ns)
+
+    types = {}
+    for ns in namespace:
+        types[ns]={}
+        print("NS%d (%s)" % (ns, namespace_array[ns]))
+        for node in nodes[ns]:
+            try:
+                types[ns][str(node.get_node_class())] += 1
+            except KeyError:
+                types[ns][str(node.get_node_class())] =1
+
+        for typ in types[ns]:
+            print("\t%s:\t%d" % (typ,types[ns][typ]))
+        print("\tTOTAL in namespace: %d" % len(nodes[ns]))
     client.disconnect()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Export Node XML from OPC UA server")
+    parser.add_argument('serverUrl', help='Complete URL of the OPC UA server', default="opc.tcp://localhost:16664")
+    parser.add_argument('-n','--namespace',
+                        metavar='<namespace>',
+                        dest="namespaces",
+                        action="append",
+                        type=int,
+                        help='Export only the given namespace index')
+    args = parser.parse_args()
+
+    export_node_xml(server_url=args.serverUrl, namespace=args.namespaces)
